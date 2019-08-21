ecc11c65d939a44e421ebeb76fa1d313b1397028
3038167125 <3038167125@qq.com>
2019-08-21 11:12:48 +0800

    111

---
+++ MVCDemo/Controllers/EFDemoController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class EFDemoController : Controller
+    {
+        MVCDemo.Models.PersonnelManagementSystemEntities db = new Models.PersonnelManagementSystemEntities();
+        // GET: EFDemo
+        public ActionResult ShowInfo()
+        {
+           var list =  db.StaffTable.ToList();
+            return View(list);
+        }
+
+       
+        public ActionResult CreatInfo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreatInfo(Models.StaffTable staff)
+        {
+            db.StaffTable.Add(staff);
+            db.SaveChanges();
+            return RedirectToAction("ShowInfo");
+        }
+
+        public ActionResult UpdateInfo(string id)
+        {
+            int staffid = Convert.ToInt32(id);
+            var obj = db.StaffTable.FirstOrDefault(o => o.StaffNum == staffid);
+            return View(obj);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateInfo(Models.StaffTable obj)
+        {
+
+            Models.StaffTable staff = db.StaffTable.FirstOrDefault(o => o.StaffNum == obj.StaffNum);
+            UpdateModel(staff);
+            db.SaveChanges();
+            return RedirectToAction("ShowInfo");
+        }
+
+
+        public ActionResult DeleteInfo(int id)
+        {
+            Models.StaffTable staff = db.StaffTable.FirstOrDefault(o => o.StaffNum == id);
+            db.StaffTable.Remove(staff);
+            db.SaveChanges();
+            return RedirectToAction("ShowInfo");
+        }
+        
+
+
+
+    }
+}
---
+++ MVCDemo/Controllers/EFbyAjaxController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class EFbyAjaxController : Controller
+    {
+        // GET: EFbyAjax
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowInfo()
+        {
+
+
+            return View();
+        }
+
+
+
+
+    }
+}
